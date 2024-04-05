e5769fd9c6835817a69b2783558e0805c3823c18
minh <nguyentrongminh1412@gmail.com>
2024-04-05 17:41:12 +0700

    update package

--- next.config.js
+++ next.config.js
@@ -1,33 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// const path = require('path');
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-// const withSass = require('@zeit/next-sass');
-
-// module.exports = withBundleAnalyzer({
-//   eslint: {
-//     dirs: ['.'],
-//   },
-//   poweredByHeader: false,
-//   trailingSlash: true,
-//   basePath: '',
-//   // The starter code load resources from `public` folder with `router.basePath` in React components.
-//   // So, the source code is "basePath-ready".
-//   // You can remove `basePath` if you don't need it.
-//   reactStrictMode: true,
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-// });
-
-// module.exports = withSass({
-//   /* additional configuration options here */
-// });
-const path = require('path');
-
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+/** @type {import('next').NextConfig} */
+ 
+const nextConfig = {
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: false,  
 };
+ 
+module.exports = nextConfig;
